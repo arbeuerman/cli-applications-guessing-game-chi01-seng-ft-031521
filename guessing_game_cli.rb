89598c432981bcbab8ec3db6bc3d3c2f21844296
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-03-14 17:50:28 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,12 @@
 # Code your solution here!
 
+def generate_random_number
+  rand(1..6)
+end 
+
 def run_guessing_game
   #generate and store a random number
+  
   #prompt user to guess a number betwen 1 and 6
   #capture user input
   #print out one of three statements based on guess 
