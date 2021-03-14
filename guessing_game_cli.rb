c1724dd9aad53113aea1f83f7957414a62aa88aa
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-03-14 18:00:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,4 @@
-# Code your solution here!
+require 'pry'
 
 def generate_random_number
   rand(5) + 1 
@@ -13,22 +13,28 @@ def get_user_input
 end 
 
 def determine_response(guess, answer)
+  binding.pry 
   if guess == answer
     puts "You guess the correct number!"
+    binding.pry 
   elsif guess == "exit"
     puts "Goodbye!"
+    binding.pry 
   else
     puts "Sorry! The computer guessed #{answer}."
+    binding.pry 
   end 
 end 
 
 def run_guessing_game
   #generate and store a random number
   answer = generate_random_number
+  binding.pry 
   #prompt user to guess a number betwen 1 and 6
   prompt_user
   #capture user input
   guess = get_user_input
+  binding.pry 
   #print out one of three statements based on guess 
   determine_response(guess, answer)
 end 
